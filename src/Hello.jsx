aa905196ffa75cb74365bb46291c96fbf27fa29a
Luca Campo <138501100+lucaprogrammer25@users.noreply.github.com>
2023-10-04 22:06:22 +0200

    Esercizio 7

---
+++ src/Hello.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const hello = () => {
+    return (
+    <h2>Hello, World!</h2>
+    )
+}
+
+export default hello;
