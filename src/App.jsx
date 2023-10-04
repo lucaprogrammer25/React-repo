f84552d1ed1419eb2fdedae743d06f4102132054
Luca Campo <138501100+lucaprogrammer25@users.noreply.github.com>
2023-10-04 18:45:50 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App({}) {
+function App() {
  
   return (
     <h1>Hello, {name}</h1>   //  In questo caso react cerca di estrarre dalla funzione l'argomento "name" ma non lo trova considerandolo non definito
