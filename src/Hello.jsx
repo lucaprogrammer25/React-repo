fe05c05c990c3c68b4b8e945701cf7e16b0b312c
Luca Campo <138501100+lucaprogrammer25@users.noreply.github.com>
2023-10-05 11:48:36 +0200

    esercizio-12

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Messaggio from './Messaggio';
 
 const hello = () => {
     return (
      <>
     <h2>Hello, World!</h2>
-    <p>Che bella giornata!</p>
+    <Messaggio />
     </>
     )
 }
