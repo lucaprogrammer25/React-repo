b511f94e26bd09cd4597f453342f60008efcf924
Luca Campo <138501100+lucaprogrammer25@users.noreply.github.com>
2023-10-06 04:03:08 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import hello from './Hello'; 
+import Hello from './Hello'; 
 
 // ...
 
 function App() {
   return (
     <div>
-      {hello()}
+      <Hello />
     </div>
   );
 }
