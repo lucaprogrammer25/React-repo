9619f1ebf9d7ecb0f1048e58e45a466287fec060
Luca Campo <138501100+lucaprogrammer25@users.noreply.github.com>
2023-10-10 19:35:46 +0200

    Add exercise 45

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,48 +1,50 @@
-import { useState } from "react"
+import React, { useState } from "react";
 
-const Login = ({stateLogin}) => {
-    const [data, setData] = useState({
-        username: '',
-        password: '',
-        remember: false
-    }
-    )
-    const handleInputChange = (event) => {
-const name = event.target.name
-const value = event.target.value 
-const checked = event.target.checked 
-const type = event.target.type
+const Login = ({ stateLogin }) => {
+  const [data, setData] = useState({
+    username: "",
+    password: "",
+    remember: false,
+  });
 
+  const handleInputChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    const checked = event.target.checked;
+    const type = event.target.type;
 
-setData((data) => {
-    return {
+    setData((data) => {
+      return {
         ...data,
-        [name] : type === 'checkbox' ? checked : value,
-    }
-})
-    }
+        [name]: type === "checkbox" ? checked : value,
+      };
+    });
+  };
 
-    const handleResetButton = () => {
-        setData({
-            username: '',
-            password: '',
-            remember: false,
-        })
-    }
+  const handleResetButton = () => {
+    setData({
+      username: "",
+      password: "",
+      remember: false,
+    });
+  };
 
- 
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-    return (
-        <>
-        <input name="username" value={data.username} onChange={handleInputChange} placeholder="username" />
-        <input name="password" value={data.password} onChange={handleInputChange} placeholder="password" />
-        <input name="remember" type="checkbox" checked={data.remember} onChange={handleInputChange} />
-        <button disabled={!data.username || !data.password} onClick={stateLogin}>Login</button>
-        <button disabled={!data.username || !data.password} onClick={handleResetButton}>Reset</button>
-        </>
-    )
-        
-    
-}
+    stateLogin()
 
-export default Login
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input name="username" value={data.username}  onChange={handleInputChange} placeholder="username"/>
+      <input name="password"   value={data.password}  onChange={handleInputChange} placeholder="password"/>
+      <input name="remember" type="checkbox" checked={data.remember} onChange={handleInputChange}/>
+      <button type="submit" disabled={!data.username || !data.password}>Login</button>
+      <button type="button" onClick={handleResetButton}>Reset</button>
+    </form>
+  );
+};
+
+export default Login;
