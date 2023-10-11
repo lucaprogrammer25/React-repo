e398ea9c7f3d19ad945dedf78daddba520fdc553
Luca Campo <138501100+lucaprogrammer25@users.noreply.github.com>
2023-10-11 16:34:17 +0200

    add exercise 49

--- src/UncontrolledLogin.jsx
+++ src/UncontrolledLogin.jsx
@@ -27,5 +27,13 @@ const UncontrolledLogin = () => {
   export default UncontrolledLogin;
 
 
-//L'API standard utilizzata è FormData ma esistono altri modi per accedere ai dati del dom, uno di questi è useRef o document.getElementById
-  
+
+/*Utilizzando la forma standard con il FormData API i vantaggi sono che :
+avremo un modo semplice e intuitivo per raccogliere i dati di un modulo, gestire automaticamente i file e gli oggetti complessi (ad esempio, array o oggetti annidati),
+questa forma è supportata da tutti i browser moderni.
+
+Svantaggi:
+La FormData API è  progettata per la raccolta di dati dai moduli HTML. Se devi raccogliere dati da fonti diverse, come oggetti JavaScript o API REST, 
+allora FormData non è l'opzione appropriata.
+ Per utilizzarlo, devi avere un riferimento al form HTML stesso.
+Non supporta dati personalizzati o strutture più complesse (ad esempio, oggetti JSON annidati).
