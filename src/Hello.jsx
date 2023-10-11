0128682f91a5c769c05f1110a55f10a6a819b00c
Luca Campo <138501100+lucaprogrammer25@users.noreply.github.com>
2023-10-11 18:41:12 +0200

    fix exercise

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -1,15 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-const hello = () => {
-    return (
-     <>
-    <h2>Hello, World!</h2>
-    <p>Che bella giornata!</p>
+const Hello = () => {
+  return (
+    <>
+      <h2>Hello, World!</h2>
+      <p>Che bella giornata!</p>
     </>
-    )
-}
+  );
+};
 
-export default hello;
+export default Hello;
