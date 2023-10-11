ab57dce9fb7559f38d114918371a8773856f8b41
Luca Campo <138501100+lucaprogrammer25@users.noreply.github.com>
2023-10-11 18:47:15 +0200

    fix esercizio-12

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -1,16 +1,17 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Message from './Message';
 import './App.css'
-import Messaggio from './Messaggio';
 
-const hello = () => {
+
+const Hello = () => {
     return (
      <>
     <h2>Hello, World!</h2>
-    <Messaggio />
+    <Message />
     </>
     )
 }
 
-export default hello;
+export default Hello;
---
+++ src/Message.jsx
@@ -0,0 +1,9 @@
+const Message = () => {
+  return (
+
+     <p>What a beautiful day!</p>
+
+  )
+};
+
+export default Message;
