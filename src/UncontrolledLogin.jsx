3b8e421e44cfee49df4911dc17df949d01254814
Luca Campo <138501100+lucaprogrammer25@users.noreply.github.com>
2023-10-11 16:20:37 +0200

    Add esercizio 48

--- src/UncontrolledLogin.jsx
+++ src/UncontrolledLogin.jsx
@@ -1,28 +1,31 @@
 const UncontrolledLogin = () => {
     const handleFormSubmit = (event) => {
-        event.preventDefault()
-
-        const formData = new FormData(event.target)
-
-        const data = {
-            username: formData.get('username'),
-            password: formData.get('password')
-        }
-
-        console.log(data)
-    }
-
-
+      event.preventDefault();
+  
+      const formData = new FormData(event.target);
+  
+      const data = {
+        form: formData.get("form"),
+        username: formData.get("username"),
+        password: formData.get("password"),
+      };
+  
+      console.log(data);
+    };
+  
     return (
-        <form onSubmit={handleFormSubmit}>
-        <input name="username"/>
+      <form name="form" onSubmit={handleFormSubmit}>
+        <input name="username" />
         <input name="password" type="password" />
         <input name="remember" type="checkbox" />
         <button>Login</button>
         <button type="reset">Reset</button>
-        </form>
-    )
-    
-}
+      </form>
+    );
+  };
+  
+  export default UncontrolledLogin;
 
-export default UncontrolledLogin
+
+//L'API standard utilizzata è FormData ma esistono altri modi per accedere ai dati del dom, uno di questi è useRef o document.getElementById
+  
