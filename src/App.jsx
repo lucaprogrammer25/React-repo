35d0b418d2da1e4b7f6c8cf087f91b086305ebd2
Luca Campo <138501100+lucaprogrammer25@users.noreply.github.com>
2023-10-11 18:54:24 +0200

    add esercizio-14

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Messaggio from './Messaggio';
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Messaggio from "./Message";
+import Hello from "./Hello";
 
 function App() {
-  
   return (
-   
-     <Messaggio/>
-   
-  )
+    <>
+      <Hello />
+      <Hello />
+      <Messaggio />;
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/Hello.jsx
+++ src/Hello.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Messaggio from './Messaggio';
+import Messaggio from "./Message";
 
-const hello = () => {
-    return (
-     <>
-    <h2>Hello, World!</h2>
-    <Messaggio />
+const Hello = () => {
+  return (
+    <>
+      <h2>Hello, World!</h2>
+      <Messaggio />
     </>
-    )
-}
+  );
+};
 
-export default hello;
+export default Hello;
