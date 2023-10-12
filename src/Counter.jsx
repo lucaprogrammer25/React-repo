22eb00461c42866f9e18b3c6bfb5674801cffc97
Luca Campo <138501100+lucaprogrammer25@users.noreply.github.com>
2023-10-12 11:32:00 +0200

    esercizio-52

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,33 +1,45 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import CounterDisplay from "./CounterDisplay";
 
 const Counter = ({initialValue, incrementAmount, decrementAmount}) => {
-    const [count, setCount] = useState(initialValue)
+    const [count, setCount] = useState(initialValue);
+    const prevCountRef = useRef(initialValue);
+    const ref = useRef("");
 
     const handleCounterIncrement = () => {
-        setCount((count) => {
-            return count + incrementAmount
-        })
+        setCount((count) => count + incrementAmount);
     }
 
     const handleCounterDecrement = () => {
-        setCount((count) => {
-            return count - decrementAmount
-        })
+        setCount((count) => count - decrementAmount);
     }
 
     const handleCounterReset = () => {
-        setCount(initialValue)
+        setCount(initialValue);
     }
+
+    useEffect(() => {
+        if (count > prevCountRef.current) {
+            ref.current = "su";
+        } else if (count < prevCountRef.current) {
+            ref.current = "giù";
+        }
+
+        if(ref.current !== prevCountRef.current) {
+            console.log(ref.current);
+        }
+
+        prevCountRef.current = count;
+    }, [count]);
+
     return (
         <>
-        <CounterDisplay content={count}/>
-        <button onClick={handleCounterIncrement}>Click me for increment!</button>
-        <button onClick={handleCounterDecrement}>Click me for decrement!</button>
-        <button onClick={handleCounterReset}>Click me for reset number!</button>
+            <CounterDisplay content={count}/>
+            <button onClick={handleCounterIncrement}>Click me for increment!</button>
+            <button onClick={handleCounterDecrement}>Click me for decrement!</button>
+            <button onClick={handleCounterReset}>Click me for reset number!</button>
         </>
     )
 }
 
 export default Counter;
-
