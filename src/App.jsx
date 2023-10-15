96b6463e14efcc3c83811bbe4473b1c75002b660
Luca Campo <138501100+lucaprogrammer25@users.noreply.github.com>
2023-10-15 21:46:09 +0200

    add esercizio-61

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 import Counter from './Counter'
+
 
 
 function App() {
   
 
   return (
-    <>
-      <Counter initialValue={0} incrementAmount={1} decrementAmount={1}/>
-    </>
+    <div className='counter'>
+      <Counter initialValue={0} incrementAmount={1} decrementAmount={1} />
+    </div>
   )
 }
 
