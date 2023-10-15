b61d66d7eb60fd939368066cfe816e3b5e45022d
Luca Campo <138501100+lucaprogrammer25@users.noreply.github.com>
2023-10-15 21:35:27 +0200

    add esercizio-60

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -1,6 +1,8 @@
 export const Welcome = (props) => {
     const {name} = props
 return (
+    <div className="welcome">
     <p>Welcome, {name} </p>
+    </div>
 )
 }
---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+const root = document.getElementById('root');
+
+const rootElement = ReactDOM.createRoot(root);
+
+rootElement.render(<App />);
+
