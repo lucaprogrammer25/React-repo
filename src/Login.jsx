e913345d91e5c8e6dc37f5fbc5ddea897484b86f
Luca Campo <138501100+lucaprogrammer25@users.noreply.github.com>
2023-10-16 12:36:04 +0200

    esercizio-62

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,35 +1,40 @@
-import { useState } from "react"
+import React, { useState } from "react";
 
 const Login = () => {
-    const [data, setData] = useState({
-        username: '',
-        password: '',
-        remember: false
-    }
-    )
-    const handleInputChange = (event) => {
-const name = event.target.name
-const value = event.target.value 
-const checked = event.target.checked 
-const type = event.target.type
+  const [data, setData] = useState({
+    username: '',
+    password: '',
+    remember: false
+  });
 
-setData((data) => {
-    return {
+  const handleInputChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    const checked = event.target.checked;
+    const type = event.target.type;
+
+    setData((data) => {
+      return {
         ...data,
-        [name] : type === 'checkbox' ? checked : value,
-    }
-})
-    }
+        [name]: type === 'checkbox' ? checked : value,
+      };
+    });
+  }
 
-    return (
-        <>
-        <input name="username" value={data.username} onChange={handleInputChange} placeholder="username" />
-        <input name="password" value={data.password} onChange={handleInputChange} placeholder="password" />
-        <input name="remember" type="checkbox" checked={data.remember} onChange={handleInputChange} />
-        </>
-    )
-        
-    
-}
+  const isPasswordValid = data.password.length >= 8;
+  const buttonStyle = {
+    backgroundColor: isPasswordValid ? "green" : "red",
+  };
 
-export default Login
+  return (
+    <>
+      <input
+        name="username" value={data.username} onChange={handleInputChange} placeholder="username"/>
+      <input name="password" value={data.password} onChange={handleInputChange} placeholder="password"/>
+      <input name="remember" type="checkbox" checked={data.remember} onChange={handleInputChange}/>
+      <button style={buttonStyle}>Login</button>
+    </>
+  );
+};
+
+export default Login;
