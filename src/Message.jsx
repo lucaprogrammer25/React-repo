c5efa8e549af62cde41204a711d56e65a1e60903
Luca Campo <138501100+lucaprogrammer25@users.noreply.github.com>
2023-10-16 19:46:16 +0200

    esercizio-13

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,8 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 const Message = () => {
     return (
     <p>What a beautiful day!</p>
