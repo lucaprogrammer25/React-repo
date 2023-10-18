71963bde5e362f47a0c9c220c333cedc92d96447
Luca Campo <138501100+lucaprogrammer25@users.noreply.github.com>
2023-10-18 12:26:03 +0200

    esercizio-29

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,19 +1,19 @@
 import { useState } from "react";
 
 const Counter = () => {
-    const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-    const handleCounterIncrement = () => {
-        setCount((count) => {
-            return count + 1
-        })
-    }
-    return (
-        <>
-        <h2>Counter:{count}</h2>
-        <button onClick={handleCounterIncrement}>Click me!</button>
-        </>
-    )
-}
+  const handleCounterIncrement = () => {
+    setCount((count) => {
+      return count + 1;
+    });
+  };
+  return (
+    <>
+      <h2>Counter:{count}</h2>
+      <button onClick={handleCounterIncrement}>Click me!</button>
+    </>
+  );
+};
 
 export default Counter;
