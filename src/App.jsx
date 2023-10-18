cc82d3a23a613365dd603b9eb870336cb2397402
Luca Campo <138501100+lucaprogrammer25@users.noreply.github.com>
2023-10-18 12:20:24 +0200

    esercizio-23

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Welcome } from './Welcome'
+import "./App.css";
+import { Welcome } from "./Welcome";
 
 function App() {
-  
-
-  return (
-    <Welcome name="Luca" age="66"/>
-
-  )
+  return <Welcome name="Luca" age="66" />;
 }
 
-export default App
+export default App;
