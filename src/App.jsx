2550d65708851b6028a4978652655db8e65b1661
Luca Campo <138501100+lucaprogrammer25@users.noreply.github.com>
2023-10-18 12:26:41 +0200

    esercizio-30

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Counter from './Counter'
-
+import "./App.css";
+import Counter from "./Counter";
 
 function App() {
-  
-
   return (
     <>
-      <Counter/>
+      <Counter />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
