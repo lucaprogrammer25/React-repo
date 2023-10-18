b76356090bc4d7aa64a9760aca05fc748c15ef0f
Luca Campo <138501100+lucaprogrammer25@users.noreply.github.com>
2023-10-18 13:55:22 +0200

    add esercizio-12

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Login from './Login'
-import { useState } from 'react'
+import Hello from './Hello'
 
 function App() {
   
 
   return (
-   <Login/>
+   <Hello/>
 
   )
 }
