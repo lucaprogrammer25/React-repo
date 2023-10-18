79576671b98a0d646b9d58153193dd7cafb81a76
Luca Campo <138501100+lucaprogrammer25@users.noreply.github.com>
2023-10-18 12:11:36 +0200

    add exercise 69

--- src/Container.jsx
+++ src/Container.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
+
 const Container = ({title, children}) => {
+    const [collapsed, setCollapsed] = useState(false)
+
+    const handleToggleCollapse = () => {
+        setCollapsed(t => !t)
+    }
+
     return (
         <div style={{
             backgroundColor: "white",
             border:"1px solid red"
         }}>
-        <div>{title}</div>
-        <div>{children}</div>
+        <div>{title} <button onClick={handleToggleCollapse}>Toggle</button></div>
+        {collapsed && <div>{children}</div>}
         </div>
     )
 }
