573950a677269fcdee707ad7cc47e1080d98cb1f
Luca Campo <138501100+lucaprogrammer25@users.noreply.github.com>
2023-10-18 12:13:12 +0200

    add exercise-19

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Welcome } from './Welcome'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Welcome } from "./Welcome";
 
 function App() {
-  
-
-  return (
-    <Welcome name="Luca"/>
-    
-  )
+  return <Welcome name="Luca" />;
 }
 
-export default App
+export default App;
--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -1,9 +1,11 @@
 export const Welcome = ({ name }) => {
-    return (
-        <p>Welcome, <strong>{name}</strong>!</p>
-    );
-  }
-  
-  export default Welcome;
+  return (
+    <p>
+      Welcome, <strong>{name}</strong>!
+    </p>
+  );
+};
 
-  //Se la prop name un'espressione jsx React la considererà parte del componente.
+export default Welcome;
+
+//Se la prop name un'espressione jsx React la considererà parte del componente.
