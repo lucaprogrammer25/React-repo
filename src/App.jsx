e43a30a87e1f914f361f12545cb4203a965ae9b2
Luca Campo <138501100+lucaprogrammer25@users.noreply.github.com>
2023-10-18 12:21:30 +0200

    esercizio-24

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Welcome } from './Welcome'
+import "./App.css";
+import { Welcome } from "./Welcome";
 
 function App() {
-  
-
-  return (
-    <Welcome name="John" age="25"/>
-
-  )
+  return <Welcome name="John" age="25" />;
 }
 
-export default App
+export default App;
--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -1,11 +1,10 @@
-import Age from './Age';
+import Age from "./Age";
 
-export const Welcome = ({ name, age}) => {
+export const Welcome = ({ name, age }) => {
   return (
     <div>
       <p>Welcome, {name} </p>
-      {(age > 18 && age < 65) && (name === "John" ) && <Age age={age} />}
+      {age > 18 && age < 65 && name === "John" && <Age age={age} />}
     </div>
   );
-}
-
+};
