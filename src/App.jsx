fc73bc5ea2afea0a9164c12c18d6e58db07bedf9
Luca Campo <138501100+lucaprogrammer25@users.noreply.github.com>
2023-10-18 14:25:51 +0200

    esercizio-19

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import "./App.css";
 import { Welcome } from "./Welcome";
 
 function App() {
-  return <Welcome name="Luca" />;
+  return <Welcome name={<span>Luca</span>}/>;
+ 
 }
 
 export default App;
