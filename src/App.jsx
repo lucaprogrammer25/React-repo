4238a3ddb9c57f5a8972f06a2bf306043e623b1f
Luca Campo <138501100+lucaprogrammer25@users.noreply.github.com>
2023-10-18 14:09:36 +0200

    esercizio-14

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Messaggio from "./Message";
+import Message from "./Message";
 import Hello from "./Hello";
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
     <>
       <Hello />
       <Hello />
-      <Messaggio />;
+      <Message />;
     </>
   );
 }
