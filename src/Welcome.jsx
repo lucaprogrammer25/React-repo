1807a188768a09037ddd45b5db4f075ffd6221db
Luca Campo <138501100+lucaprogrammer25@users.noreply.github.com>
2023-10-18 12:22:13 +0200

    esercizio-25

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -1,15 +1,10 @@
-import Age from './Age';
+import Age from "./Age";
 
-export const Welcome = ({ name, age}) => {
+export const Welcome = ({ name, age }) => {
   return (
     <div>
       <p>Welcome, {name} </p>
-      {age > 18 ? (
-        <p>Your age is {age}</p>
-      ) : (
-        <p>You are very young!</p>
-      )}
+      {age > 18 ? <p>Your age is {age}</p> : <p>You are very young!</p>}
     </div>
   );
-}
-
+};
