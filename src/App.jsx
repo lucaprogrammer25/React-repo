ed5090a950c7ae86e009eb7b52561a9d36ce50b2
Luca Campo <138501100+lucaprogrammer25@users.noreply.github.com>
2023-11-03 15:38:32 +0100

    esercizio-84

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import {Routes, Route, Link} from "react-router-dom"
 import ShowGithubUser from "./ShowGithubUser";
 import { Welcome } from "./Welcome";
 import Counter from "./Counter";
+import NotFound from "./NotFound";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Link to="/counter" element={<Counter/>}>Counter</Link>
     </nav>
     <Routes>
+       <Route path="*" element={<NotFound />} />
        <Route exact path="/" element={<Welcome name="Luca"/>} />
        <Route path="/users/:username" element={<ShowGithubUser/>}/>
        <Route path="/counter" element={<Counter/>}/>
