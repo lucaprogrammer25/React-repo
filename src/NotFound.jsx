2d4893ebe515eab86039455de341f3e4cfe65316
Luca Campo <138501100+lucaprogrammer25@users.noreply.github.com>
2023-11-04 16:10:45 +0100

    esercizio-84

--- src/NotFound.jsx
+++ src/NotFound.jsx
@@ -4,7 +4,7 @@ const NotFound = () => {
   return (
     <div>
       <h1>Not Found</h1>
-      <p>This page don't exist</p>
+      <p>This page doesn't exist</p>
       <Link to="/">Home</Link>
     </div>
   );
