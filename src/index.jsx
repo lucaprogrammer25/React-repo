90f3709ecd3e965aadeebfb7b7e095d423b24e30
Luca Campo <138501100+lucaprogrammer25@users.noreply.github.com>
2023-11-07 19:36:54 +0100

    esercizio-90

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import { SWRConfig } from 'swr';
 import {BrowserRouter} from "react-router-dom"
+import './index.css'
+
+const fetcher = url => fetch(url).then(res => res.json());
 
 const root = document.getElementById('root');
 
 const rootElement = ReactDOM.createRoot(root);
 
 rootElement.render(
+    <SWRConfig value={{ fetcher }}>
     <BrowserRouter>
-        <App />
+      <App />
     </BrowserRouter>
+  </SWRConfig>
 );
 
