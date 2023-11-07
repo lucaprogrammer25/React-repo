80a22f2285b082e14490757d4cfdde2d891b8dbe
Luca Campo <138501100+lucaprogrammer25@users.noreply.github.com>
2023-11-07 19:17:56 +0100

    esercizio87

--- src/useGithubuser.js
+++ src/useGithubuser.js
@@ -1,11 +1,14 @@
 import useSWR from 'swr'
 const fetcher = (url) => fetch(url).then(response => response.json());
-const useGithubUser = (API_URL) => {
+const useGithubUser = (API_URL, username) => {
+  if(username) {
+    const { data, error} = useSWR(API_URL, fetcher)
+    return {data, error}
+  }
+  
 
-  const { data, error} = useSWR(API_URL, fetcher)
 
-
-  return { data, error}
+  return { data: null, error: null}
 
 }
 
